0c6e04b4b986dbf4a00a7df9669bf4ce5a5a28a1
heybrettbell <brett@grassrootsonline.ca>
2017-08-11 14:30:16 -0400

    Assignment complete

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,22 +1,30 @@
 class PokemonController < ApplicationController
   def show
     res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
-    # byebug
+
     body = JSON.parse(res.body)
     name  = body["name"]
     id = body["id"]
     types = body["types"]
     result = []
-    types.each do |type|
-      diff_types = type["type"]["name"]
-      result << diff_types
-    end
+      types.each do |type|
+        diff_types = type["type"]["name"]
+        result << diff_types
+      end
+
+    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{name}&rating=g")
+    body = JSON.parse(res.body)
+    # byebug
+    url = body["data"][0]["url"]
 
     render json: {
       "id": id,
       "name": name,
-      "types": result
+      "types": result,
+      "url": url
     }
 
   end
+
+
 end
